2615bc63ad8167f89c5563cbb830b8976989f757
namplt-dev <namplt.work@gmail.com>
2026-05-20 22:11:22 +0700

    Update: largest-rectangle-in-histogram - submission-1

--- Data Structures & Algorithms/largest-rectangle-in-histogram/submission-1.cs
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-1.cs
@@ -1,13 +1,23 @@
 public class Solution {
     public int LargestRectangleArea(int[] heights) {
         int res = 0;
+        Stack<int[]> stack = new Stack<int[]>();
         for (int i = 0; i < heights.Length; i++) {
-            int min = heights[i];
-            res = Math.Max(res, heights[i]);
-            for (int j = i + 1; j < heights.Length; j++) {
-                min = Math.Min(min, heights[j]);
-                res = Math.Max(res, min * (j - i + 1));
+            int start = i;
+            while (stack.Count > 0 && stack.Peek()[1] > heights[i]) {
+                int[] top = stack.Pop();
+                int index = top[0];
+                int height = top[1];
+                res = Math.Max(res, height * (i - index));
+                start = index;
             }
+            stack.Push(new int[] { start, heights[i] });
+        }
+
+        foreach (var p in stack) {
+            int index = p[0];
+            int height = p[1];
+            res = Math.Max(res, height * (heights.Length - index));
         }
         return res;
     }
