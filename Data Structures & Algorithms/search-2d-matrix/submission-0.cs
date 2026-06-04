514db5854251448a2c921feefd0489197abad3ac
namplt-dev <namplt.work@gmail.com>
2026-06-04 22:28:59 +0700

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int len = matrix[0].Length * matrix.Length - 1;
+
+        return Search(matrix, 0, len, target);
+    }
+
+    public bool Search(int[][] matrix, int l, int r, int target) {
+        if (l > r) {
+            return false;
+        }
+        int mid = l + (r - l) / 2;
+        int row = mid / matrix[0].Length;
+        int col = mid % matrix[0].Length;
+
+        if (matrix[row][col] == target) {
+            return true;
+        }
+
+        int index = row * matrix[0].Length + col;
+        return (matrix[row][col] > target) ? Search(matrix, 0, index - 1, target)
+                                           : Search(matrix, index + 1, r, target);
+    }
+}
